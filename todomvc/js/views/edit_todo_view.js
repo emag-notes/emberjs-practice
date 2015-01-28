8de5cc84b2cb4b421be2103e71964d16ed66e190
emag <lanabe.lanabe@gmail.com>
2015-01-28 13:48:22 +0900

    Accepting edits

---
+++ todomvc/js/views/edit_todo_view.js
@@ -0,0 +1,9 @@
+'use strict';
+
+Todos.EditTodoView = Ember.TextField.extend({
+  didInsertElement: function() {
+    this.$().focus();
+  }
+});
+
+Ember.Handlebars.helper('edit-todo', Todos.EditTodoView);
