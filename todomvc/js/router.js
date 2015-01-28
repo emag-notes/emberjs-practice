780af5fc2f2500edc452e338250336bdcad9fda9
emag <lanabe.lanabe@gmail.com>
2015-01-28 10:03:34 +0900

    Display Model Data

--- todomvc/js/router.js
+++ todomvc/js/router.js
@@ -3,3 +3,9 @@
 Todos.Router.map(function () {
   this.resource('todos', {path: '/'});
 });
+
+Todos.TodosRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('todo');
+  }
+});
